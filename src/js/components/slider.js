616d07f57623c5df1550073131d76395e2eec418
Andrey <exzy92@gmail.com>
2020-11-04 22:36:18 +0300

    media for works section, laptop

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -4,12 +4,24 @@ import vars from '../_vars';
 Swiper.use([Navigation, Pagination]);
 
 const worksSlider = new Swiper(vars.$worksSlider, {
-  spaceBetween: 30,
-  slidesPerView: 3,
+  spaceBetween: 36,
+  slidesPerView: 2,
   loop: true,
 
   navigation: {
     prevEl: '.swiper-button-prev',
     nextEl: '.swiper-button-next',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   },
 });
