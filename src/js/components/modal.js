f9a2dfeb51b7d192ace66687ab75e8428b11a063
Andrey <exzy92@gmail.com>
2020-12-25 16:16:31 +0300

    animate modal

--- src/js/components/modal.js
+++ src/js/components/modal.js
@@ -1,56 +1,69 @@
-import vars from '../_vars';
-
 'use strict';
+// vars
+const body = $('.js-body');
+const modal = $('.js-modal');
+const modalOverlay = $('.js-modal-overlay');
+const openModalBtns = $('.js-open-modal');
+const closeModalBtn = $('.js-modal-close');
+const cancelModalBtn = $('.js-form-cancel');
+const modalInputArea = $('.js-modal-input');
 
-const cleaningModalInputArea = () => {
-  vars.$modalInputArea.forEach(input => {
-    input.value = '';
-  });
-}
-
+// functions
 const showModal = () => {
-  vars.$modal.classList.remove('is-hidden');
-  vars.$modal.setAttribute('aria-hidden', false);
-  vars.$modalOverlay.classList.remove('is-hidden');
+  modal.attr('display', 'block');
+  modalOverlay.attr('display', 'block');
+  modalOverlay.fadeIn('slow');
+  modal.fadeIn('slow');
+  modal.attr('aria-hidden', false);
 }
 
 const hideModal = () => {
-  vars.$modal.classList.add('is-hidden');
-  vars.$modal.setAttribute('aria-hidden', true);
-  vars.$modalOverlay.classList.add('is-hidden');
+  modalOverlay.fadeOut('slow');
+  modal.fadeOut('slow');
+  modal.attr('display', 'none');
+  modalOverlay.attr('display', 'none');
+  modal.attr('aria-hidden', true);
+}
+
+const cleaningModalInputArea = () => {
+  modalInputArea.each((i) => {
+    modalInputArea[i].value = '';
+  })
 }
 
 // open modal window
-vars.$openModalBtns.forEach(btn => {
-  btn.addEventListener('click', (e) => {
+openModalBtns.each((i) => {
+  openModalBtns[i].addEventListener('click', (e) => {
     e.preventDefault();
-
     showModal();
   })
 });
 
+// close modal window
+// click on close btn
+closeModalBtn.click((e) => {
+  e.preventDefault();
+  hideModal();
+})
 
-// hide modal window
-vars.$modalOverlay.addEventListener('click', () => {
+// click on cancel btn
+cancelModalBtn.click((e) => {
+  e.preventDefault();
   hideModal();
   cleaningModalInputArea();
-});
+})
 
-vars.$closeModalBtn.addEventListener('click', () => {
-  hideModal();
-  cleaningModalInputArea();
-});
-
-vars.$cancelFormBtn.addEventListener('click', () => {
-  hideModal();
-  cleaningModalInputArea();
-});
-
-vars.$document.addEventListener('keyup', (e) => {
+// click on key 'Esc'
+body.keyup((e) => {
   e.preventDefault();
 
   if (e.key === 'Escape') {
     hideModal();
     cleaningModalInputArea();
   }
+})
+
+// click on area != modal
+modalOverlay.click(() => {
+  hideModal();
 })
