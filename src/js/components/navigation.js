93bce9c1332229b4b348731c27bb973964e504a8
Andrey Dvortsov <61544631+dushmanexzy@users.noreply.github.com>
2021-01-12 14:23:30 +0300

    correct open\close navigation code

--- src/js/components/navigation.js
+++ src/js/components/navigation.js
@@ -1,11 +1,34 @@
 import vars from '../_vars';
 
+const closeNavList = function () {
+  vars.$header.classList.remove('menu-open');
+}
+
 vars.$window.addEventListener('DOMContentLoaded', () => {
   vars.$body.classList.remove('no-js');
 
   vars.$toggleNavBtn.addEventListener('click', (e) => {
     e.preventDefault();
-
     vars.$header.classList.toggle('menu-open');
+  })
+
+  vars.$document.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (!vars.$header.contains(e.target)) {
+      closeNavList();
+    }
+
+    if (e.target.classList.contains('js-anchor')) {
+      closeNavList();
+    }
+
+    vars.$body.addEventListener('keyup', (e) => {
+      e.preventDefault();
+
+      if (e.key === 'Escape') {
+        closeNavList();
+      }
+    })
   })
 })
